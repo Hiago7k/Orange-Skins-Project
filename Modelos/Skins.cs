e846772c4474cc06aad13cc4ae2c5e44cd5a5004
Hiago 7k <hiago7k@gmail.com>
2026-03-26 20:10:08 -0300

    criando classe skins, com proprieades

---
+++ Modelos/Skins.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Orange_Skins_Project.Modelos;
+
+internal class Skins
+{
+    [JsonPropertyName("name")]
+    public string? NomeDaSkin { get;}
+    [JsonPropertyName("description")]
+    public string? DescricaoDaSkin { get;}
+
+    [JsonPropertyName("id")]
+    public string? Id { get;}
+    [JsonPropertyName("category")]
+    public string? Categoria { get;}
+}
